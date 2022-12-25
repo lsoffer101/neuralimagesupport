0385d4c49b134c547ea6fcf6a362d80e7c0b788d
Lucas Soffer <lucassoffer@mac-43A097.local>
2022-12-25 10:39:16 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='neuralimagesupport',
+      version='0.0.1',
+      author='Lucas Soffer',
+      author_email='lucas.soffer1@gmail.com',
+      install_requires=[
+          "numpy >= 1.20",
+        #   "scipy >= 1.6.0",
+          "pandas >= 1.3.4",
+      ])
+    #   entry_points={
+    #       'console_scripts': [
+    #           'vantage=vantagenis:main',
+    #       ],
+    #   })
